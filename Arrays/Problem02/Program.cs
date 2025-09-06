792ec4f5a7dbe7c0945f6ab7d050e1b4dce3c848
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-06 13:04:27 +0530

    Rotate an array k steps to the right

---
+++ Arrays/Problem02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Rotate an array k steps to the right
+
+using System;
+
+class Program {
+  static void Rotate(int[] nums, int k) {
+    k = k % nums.Length;
+    Array.Reverse(nums);
+    Array.Reverse(nums, 0, k);
+    Array.Reverse(nums, k, nums.Length - k);
+  }
+
+  static void Main() {
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    Rotate(arr, k);
+    Console.WriteLine(string.Join(" ", arr));
+  }
+}
