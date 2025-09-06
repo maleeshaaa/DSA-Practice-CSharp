fef235178621f234365635b51d3e87c14b2f5fd7
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-06 10:55:56 +0530

    Project Setup

---
+++ Arrays/Problem01/Program.cs
@@ -0,0 +1,35 @@
+﻿// Find the second largest element in an array
+
+using System;
+
+class EX01
+{
+  static void Main()
+  {
+    int[] arr = { 12, 35, 1, 10, 34, 1 };
+    int firstLargest = int.MinValue;
+    int secondLargest = int.MinValue;
+
+    foreach (int num in arr)
+    {
+      if (num > firstLargest)
+      {
+        secondLargest = firstLargest;
+        firstLargest = num;
+      }
+      else if (num > secondLargest && num != firstLargest)
+      {
+        secondLargest = num;
+      }
+    }
+
+    if (secondLargest == int.MinValue)
+    {
+      Console.WriteLine("There is no second largest element.");
+    }
+    else
+    {
+      Console.WriteLine("The second largest element is: " + secondLargest);
+    }
+  }
+}
