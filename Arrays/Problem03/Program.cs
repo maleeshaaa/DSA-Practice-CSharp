6107e0c8a2f1eda3b53a55eb8dc2d29253de6369
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-06 13:33:21 +0530

    Find the missing number in a sequence 1…n

---
+++ Arrays/Problem03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Find the missing number in a sequence 1…n
+
+using System;
+
+class Program
+{
+  static void Main()
+  {
+    int[] arr = [2, 3, 1, 5];
+    int n = arr.Length + 1;
+    int total = n * (n + 1) / 2;
+    int sum = 0;
+
+    foreach (int num in arr)
+      sum += num;
+
+    int missingNumber = total - sum;
+    Console.WriteLine("Missing Number: " + missingNumber);
+  }
+}
