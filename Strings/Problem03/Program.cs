ae18a304973253ebb668a2d55a3cb63b95dbc35a
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-07 11:43:22 +0530

    Reverse words in a sentence

---
+++ Strings/Problem03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Reverse words in a sentence
+
+using System;
+
+class Program
+{
+  static string ReverseSentence(string sentence)
+  {
+    string[] words = sentence.Split(' ');
+    Array.Reverse(words);
+    return string.Join(" ", words);
+  }
+
+  static void Main(string[] args)
+  {
+    Console.Write("Enter a sentence: ");
+    string input = Console.ReadLine();
+    string reversedStr = ReverseSentence(input);
+    Console.WriteLine("Reversed sentence: " + reversedStr);
+  }
+}
