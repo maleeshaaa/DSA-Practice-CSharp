5673667f0628ee379dfc608cd0d3531914cd6184
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-07 12:27:48 +0530

    Count vowels and consonants

---
+++ Strings/Problem05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+  static void CountVowelsAndConsonants(string input, out int vowelCount, out int consonantsCount)
+  {
+    vowelCount = 0;
+    consonantsCount = 0;
+    string vowels = "aeiouAEIOU";
+
+    foreach (char c in input)
+    {
+      if (char.IsLetter(c))
+      {
+        if (vowels.Contains(c))
+        {
+          vowelCount++;
+        }
+        else
+        {
+          consonantsCount++;
+        }
+      }
+    }
+  }
+
+  static void Main()
+  {
+    Console.Write("Enter a string: ");
+    string input = Console.ReadLine();
+
+    CountVowelsAndConsonants(input, out int vowels, out int consonants);
+    
+    Console.WriteLine($"Vowels: {vowels}, Consonants: {consonants}");
+  }
+}
