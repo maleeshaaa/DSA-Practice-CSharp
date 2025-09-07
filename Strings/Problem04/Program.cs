781a5e68446246dde4d257a9580896b517dafe67
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-07 12:02:23 +0530

    Check if a string is a valid palindrome

---
+++ Strings/Problem04/Program.cs
@@ -0,0 +1,29 @@
+﻿// Check if a string is a valid palindrome
+
+using System;
+
+class Program
+{
+  static bool IsPalindrome(string str)
+  {
+    char[] chars = str.ToCharArray();
+    Array.Reverse(chars);
+    string newStr = new string(chars);
+    return str == newStr;
+  }
+
+  static void Main(string[] args)
+  {
+    Console.Write("Enter a string: ");
+    string input = Console.ReadLine();
+
+    if (IsPalindrome(input))
+    {
+      Console.WriteLine($"\"{input}\" is a palindrome.");
+    }
+    else
+    {
+      Console.WriteLine($"\"{input}\" is not a palindrome.");
+    }
+  }
+}
