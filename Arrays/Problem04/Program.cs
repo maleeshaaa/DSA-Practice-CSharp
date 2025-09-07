1832a60b06da60dc6278bd0d56593a1474212283
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-07 09:47:00 +0530

    Move all zeros to the end without using extra space

---
+++ Arrays/Problem04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Move all zeros to the end without using extra space
+
+using System;
+
+class Program
+{
+  static void Main()
+  {
+    int[] arr = { 0, 1, 2, 0, 3, 4 };
+    int index = 0;
+
+    foreach (int num in arr)
+    {
+      if (num != 0)
+      {
+        arr[index++] = num;
+      }
+    }
+
+    while (index < arr.Length)
+    {
+      arr[index++] = 0;
+    }
+
+    Console.WriteLine(string.Join(" ", arr));
+  }
+}
