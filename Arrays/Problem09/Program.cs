bafcf962a417dfd0305c1e8e0fc5e23824bf5c13
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-11 12:10:37 +0530

    Find the intersection of two arrays without duplicates

---
+++ Arrays/Problem09/Program.cs
@@ -0,0 +1,45 @@
+﻿// Find the intersection of two arrays, returning a new array with unique elements only and another with duplicates allowed.
+// For example, given arrays [1, 2, 2, 3] and [2, 2, 3, 4], the unique intersection is [2, 3] and the intersection with duplicates is [2, 2, 3].
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+  static int[] IntersectionWithoutDuplicates(int[] array1, int[] array2)
+  {
+    HashSet<int> set1 = new HashSet<int>(array1);
+    HashSet<int> set2 = new HashSet<int>(array2);
+    set1.IntersectWith(set2);
+    return set1.ToArray();
+  }
+
+  static int[] IntersectionWithoutDuplicates_Manual(int[] array1, int[] array2)
+  {
+    HashSet<int> set1 = new HashSet<int>(array1);
+    HashSet<int> resultSet = new HashSet<int>();
+
+    foreach (int num in array2)
+    {
+      if (set1.Contains(num))
+      {
+        resultSet.Add(num);
+      }
+    }
+
+    return resultSet.ToArray();
+  }
+
+  static void Main(string[] args)
+  {
+    int[] array1 = { 1, 2, 2, 3, 4, 4 };
+    int[] array2 = { 3, 4, 4, 5, 6 };
+
+    int[] result = IntersectionWithoutDuplicates(array1, array2);
+    Console.WriteLine("Intersection without duplicates: " + string.Join(", ", result));
+
+    int[] resultManual = IntersectionWithoutDuplicates_Manual(array1, array2);
+    Console.WriteLine("Intersection without duplicates (Manual): " + string.Join(", ", resultManual));
+  }
+}
