82bfa3c7341f7a19c99578346f3909bfe57038f7
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-11 12:43:36 +0530

    Find the longest common prefix string

---
+++ Strings/Problem08/Program.cs
@@ -0,0 +1,32 @@
+﻿// Find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+
+using System;
+
+class Program
+{
+  static string FindLongestCommonPrefix(string[] strs)
+  {
+    if (strs == null || strs.Length == 0) return "";
+
+    string prefix = strs[0];
+
+    for (int i = 1; i < strs.Length; i++)
+    {
+      while (!strs[i].StartsWith(prefix))
+      {
+        prefix = prefix.Substring(0, prefix.Length - 1);
+        if (string.IsNullOrEmpty(prefix)) return "";
+      }
+    }
+
+    return prefix;
+  }
+  
+  static void Main(string[] args)
+  {
+    string[] strs = { "flower", "flow", "flight" };
+    string result = FindLongestCommonPrefix(strs);
+    Console.WriteLine($"Longest common prefix: {result}");
+  }
+}
