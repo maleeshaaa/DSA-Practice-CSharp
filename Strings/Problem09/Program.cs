aac42b187a03a3ae650eaeae45c9b9f538a0548f
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-09-12 09:23:11 +0530

    String Rotation Check

---
+++ Strings/Problem09/Program.cs
@@ -0,0 +1,26 @@
+﻿// String Rotation Check
+
+using System;
+
+class Program
+{
+  static bool IsRotationString(string str1, string str2)
+  {
+    if (str1.Length != str2.Length)
+    {
+      return false;
+    }
+
+    string concatenated = str1 + str1;
+    return concatenated.Contains(str2);
+  }
+
+  static void Main()
+  {
+    string str1 = "waterbottle";
+    string str2 = "erbottlewat";
+
+    bool result = IsRotationString(str1, str2);
+    Console.WriteLine($"Is '{str2}' a rotation of '{str1}'? {result}");
+  }
+}
